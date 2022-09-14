51a2151267622d10bd8bbd24d071cccf50689a1b
paraschandra <paraschandra30@gmail.com>
2022-09-14 19:42:14 +0530

    init

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = document.querySelector('.main');
-
 const temp = document.querySelector('.temp');
+const s = "ed";
 const dateOutput = document.querySelector('.date');
 const timeOutput = document.querySelector('.time');
 const inputLocation = document.querySelector('.location');
 const condition = document.querySelector('.condition');
 const icon = document.querySelector('.icon');
+const feelsLike = document.querySelector('.feels-like');
 const cloud = document.querySelector('.cloud');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
@@ -13,6 +14,7 @@ const form = document.getElementById('inputLocation');
 const search = document.querySelector('.search');
 const btn = document.querySelector('.submit');
 const cities = document.querySelectorAll('.city');
+const API_KEY = `fcf5bdd2944a344a1${s}3a8d3b1c11fd1`;
 
 function whiteIcon(){
     document.getElementById("submit").src = "./assets/icons/search_white.svg";
@@ -22,13 +24,13 @@ function blackIcon(){
     document.getElementById("submit").src = "./assets/icons/search_black.svg";
 }
 
-let cityInput = "London";
+let cityInput = "New Delhi";
 
 cities.forEach((city) => {
     city.addEventListener('click', (e) => {
         cityInput = e.target.innerHTML;
         getWeather();
-        // app.style.opacity = "0";
+        app.style.opacity = "0";
     })
 });
 
@@ -40,7 +42,7 @@ form.addEventListener('submit', (e) => {
         cityInput = search.value;
         getWeather();
         search.value = "";
-        // app.style.opacity = "0";
+        app.style.opacity = "0";
     }
     e.preventDefault();
 });
@@ -78,6 +80,7 @@ function getWeather(){
 
             document.getElementById('icon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
+            feelsLike.innerHTML = Math.round(data.main.feels_like) + "&#176";
             cloud.innerHTML = data.clouds.all + "%";
             humidity.innerHTML = data.main.humidity + "%";
             wind.innerHTML = data.wind.speed + "km/h";
@@ -98,16 +101,42 @@ function getWeather(){
                     btn.style.background = "#323a70";
                 }
             }
-            else if(code == 801 || code == 802 || code == 803 || code == 804){
+            else if(code == 801 || code == 802 || code == 803 || code == 804 || code == 781){
                 app.style.backgroundImage = `url(./assets/images/${timeOfDay}/cloudy.jpg)`;
                 btn.style.background = "#fa6d1b";
                 if(timeOfDay == "night"){
-                    btn.style.background = "#496980";
+                    btn.style.background = "#162739";
                 }
             }
-
+            else if(code == 200 || code == 201 || code == 202 || code == 210 || code == 211 || code == 212 || code == 221 || code == 230 || code == 231 || code == 232 || code == 300 || code == 301 || code == 302 || code == 310 || code == 311 || code == 312 || code == 313 || code == 314 || code == 321 || code == 500 || code == 501 || code == 502 || code == 503 || code == 504 || code == 511 || code == 520 || code == 521 || code == 522 || code == 531){
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/rain.jpg)`;
+                btn.style.background = "#829f9a";
+                if(timeOfDay == "night"){
+                    btn.style.background = "#182f43";
+                }
+            }
+            else if(code == 701 || code == 711 || code == 721 || code == 731 || code == 741 || code == 751 || code == 761 || code == 762 || code == 771){
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/atmosphere.jpg)`;
+                btn.style.background = "#3e4038";
+                if(timeOfDay == "night"){
+                    btn.style.background = "#1d5f7b";
+                }
+            }
+            else{
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/snow.jpg)`;
+                btn.style.background = "#80b9cc";
+                if(timeOfDay == "night"){
+                    btn.style.background = "#838c95";
+                }
+            }
+            app.style.opacity = "1";
         })
-}
 
-// app.style.opacity = "1";
+        .catch(() => {
+            alert('City not found! Try again');
+            app.style.opacity = "1";
+        });
+}
 getWeather();
+
+app.style.opacity = "1";
