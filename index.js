fc8b22cf9ff5dc48fa9313f6324a793655d79722
paraschandra <paraschandra30@gmail.com>
2023-02-06 14:32:49 +0530

    init

--- index.js
+++ index.js
@@ -95,35 +95,35 @@ function getWeather(){
             }
             
             if(code == 800){
-                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/clear.jpg)`;
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/clear.webp)`;
                 btn.style.background = "#4798d5";
                 if(timeOfDay == "night"){
                     btn.style.background = "#323a70";
                 }
             }
             else if(code == 801 || code == 802 || code == 803 || code == 804 || code == 781){
-                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/cloudy.jpg)`;
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/cloudy.webp)`;
                 btn.style.background = "#fa6d1b";
                 if(timeOfDay == "night"){
                     btn.style.background = "#162739";
                 }
             }
             else if(code == 200 || code == 201 || code == 202 || code == 210 || code == 211 || code == 212 || code == 221 || code == 230 || code == 231 || code == 232 || code == 300 || code == 301 || code == 302 || code == 310 || code == 311 || code == 312 || code == 313 || code == 314 || code == 321 || code == 500 || code == 501 || code == 502 || code == 503 || code == 504 || code == 511 || code == 520 || code == 521 || code == 522 || code == 531){
-                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/rain.jpg)`;
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/rain.webp)`;
                 btn.style.background = "#829f9a";
                 if(timeOfDay == "night"){
                     btn.style.background = "#182f43";
                 }
             }
             else if(code == 701 || code == 711 || code == 721 || code == 731 || code == 741 || code == 751 || code == 761 || code == 762 || code == 771){
-                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/atmosphere.jpg)`;
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/atmosphere.webp)`;
                 btn.style.background = "#3e4038";
                 if(timeOfDay == "night"){
                     btn.style.background = "#1d5f7b";
                 }
             }
             else{
-                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/snow.jpg)`;
+                app.style.backgroundImage = `url(./assets/images/${timeOfDay}/snow.webp)`;
                 btn.style.background = "#80b9cc";
                 if(timeOfDay == "night"){
                     btn.style.background = "#838c95";
